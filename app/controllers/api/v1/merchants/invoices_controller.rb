6958b74704e68a84bce6080cffeee48a4be737e3
Jim Macur <jim.macur@gmail.com>
2024-09-22 19:12:33 -0600

    test/feat: adds def show and tests

--- app/controllers/api/v1/merchants/invoices_controller.rb
+++ app/controllers/api/v1/merchants/invoices_controller.rb
@@ -10,10 +10,46 @@ class Api::V1::Merchants::InvoicesController < ApplicationController
     render json: InvoiceSerializer.new(invoices)
   end
 
-  def show
-    merhant = merchant.find(params[:mercahnt_id])
-    invoice = merchant.invoice.finnd(params[:id])
 
-    render json: InvoiceSerializer.new(invoice)
+  def show
+    invoice = Invoice.find_by(id: params[:id], merchant_id: params[:merchant_id])
+    if invoice
+      render json: InvoiceSerializer.new(invoice)
+    else
+      render json: { errors: "Invoice not found" }, status: :not_found
+    end
   end
 end
+
+private
+
+def invoice_params
+  params.require(:invoice).permit(:amount, :merchant_id, :status, :coupon_id)
+end
+
+#   def create
+#     merchant = Merchant.find(params[:merchant_id])
+#     invoice = merchant.invoices.new(invoice_params)
+#     if invoice.save
+#       render json: InvoiceSerializer.new(invoice), status: :created
+#     else
+#       render json: { errors: invoice.errors.full_messages }, status: :unprocessable_entity
+#     end
+#   end
+  
+#   def update 
+#     invoice = Invoice.find(params[:id])
+
+#     if invoice.update(invoice_params)
+#       render json: InvoiceSerializer.new(invoice)
+#     else
+#       render_error(invoice)
+#     end
+#   end
+
+
+
+#   def render_error(invoice)
+#     render json: { errors: invoice.errors.full_messages }, status: :unprocessable_entity
+#   end
+# end
--- spec/requests/api/v1/merchant/invoices_request_spec.rb
+++ spec/requests/api/v1/merchant/invoices_request_spec.rb
@@ -8,62 +8,72 @@ RSpec.describe "Merchant invoices endpoints" do
     @customer1 = create(:customer)
     @customer2 = create(:customer)
 
-    @invoice1 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "packaged")
+    @coupon = create(:coupon, merchant: @merchant1)
+
+    @invoice1 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "packaged", coupon_id: @coupon.id)
     create_list(:invoice, 3, merchant_id: @merchant1.id, customer_id: @customer1.id) # shipped by default
     @invoice2 = Invoice.create!(customer: @customer1, merchant: @merchant2, status: "shipped")
   end
 
-  it "should return all invoices for a given merchant based on status param" do
-    get "/api/v1/merchants/#{@merchant1.id}/invoices?status=packaged"
+  describe "GET /api/v1/merchants/:merchant_id/invoices without params" do
+    it "should return all invoices for merchant if no params" do
+      get "/api/v1/merchants/#{@merchant1.id}/invoices"
 
-    json = JSON.parse(response.body, symbolize_names: true)
+      json = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-    expect(json[:data].count).to eq(1)
-    expect(json[:data][0][:id]).to eq(@invoice1.id.to_s)
-    expect(json[:data][0][:type]).to eq("invoice")
-    expect(json[:data][0][:attributes][:customer_id]).to eq(@customer1.id)
-    expect(json[:data][0][:attributes][:merchant_id]).to eq(@merchant1.id)
-    expect(json[:data][0][:attributes][:status]).to eq("packaged")
+      expect(response).to be_successful
+      expect(json[:data].count).to eq(4)
+    end
   end
 
-  it "should get multiple invoices if they exist for a given merchant and status param" do
-    get "/api/v1/merchants/#{@merchant1.id}/invoices?status=shipped"
+  describe "GET /api/v1/merchants/:merchant_id/invoices with status param" do
+    it "should return all invoices for a given merchant based on status param" do
+      get "/api/v1/merchants/#{@merchant1.id}/invoices?status=packaged"
 
-    json = JSON.parse(response.body, symbolize_names: true)
+      json = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-    expect(json[:data].count).to eq(3)
-  end
-
-  it "should only get invoices for merchant given" do
-    get "/api/v1/merchants/#{@merchant2.id}/invoices?status=shipped"
-
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    expect(response).to be_successful
-    expect(json[:data].count).to eq(1)
-    expect(json[:data][0][:id]).to eq(@invoice2.id.to_s)
-  end
+      expect(response).to be_successful
+      expect(json[:data].count).to eq(1)
+      expect(json[:data][0][:id]).to eq(@invoice1.id.to_s)
+      expect(json[:data][0][:type]).to eq("invoice")
+      expect(json[:data][0][:attributes][:customer_id]).to eq(@customer1.id)
+      expect(json[:data][0][:attributes][:merchant_id]).to eq(@merchant1.id)
+      expect(json[:data][0][:attributes][:status]).to eq("packaged")
+    end
   
-  it "should return all invoices for merchant if no params" do
-    get "/api/v1/merchants/#{@merchant1.id}/invoices"
+    it "should get multiple invoices if they exist for a given merchant and status param" do
+      get "/api/v1/merchants/#{@merchant1.id}/invoices?status=shipped"
 
-    json = JSON.parse(response.body, symbolize_names: true)
+      json = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to be_successful
-    expect(json[:data].count).to eq(4)
+      expect(response).to be_successful
+      expect(json[:data].count).to eq(3)
+    end
   end
 
-  it "should return 404 and error message when merchant is not found" do
-    get "/api/v1/merchants/100000/customers"
+  describe "GET /api/v1/merchants/:merchant_id/invoices for a specific merchant" do
+    it "should only get invoices for merchant given" do
+      get "/api/v1/merchants/#{@merchant2.id}/invoices?status=shipped"
 
-    json = JSON.parse(response.body, symbolize_names: true)
+      json = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response).to have_http_status(:not_found)
-    expect(json[:message]).to eq("Your query could not be completed")
-    expect(json[:errors]).to be_a Array
-    expect(json[:errors].first).to eq("Couldn't find Merchant with 'id'=100000")
+      expect(response).to be_successful
+      expect(json[:data].count).to eq(1)
+      expect(json[:data][0][:id]).to eq(@invoice2.id.to_s)
+    end
+  end
+
+  describe "GET /api/v1/merchants/invalid_merchant_id" do
+    it "returns 404 and error message when merchant is not found" do
+      get "/api/v1/merchants/100000/customers"
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:not_found)
+      expect(json[:message]).to eq("Your query could not be completed")
+      expect(json[:errors]).to be_a Array
+      expect(json[:errors].first).to eq("Couldn't find Merchant with 'id'=100000")
+    end
   end
 
   describe 'GET /api/v1/merchants/:merchant_id/invoices' do
@@ -97,6 +107,29 @@ RSpec.describe "Merchant invoices endpoints" do
       data = json_response[:data]
   
       expect(data).to be_empty
+    end
+  end
+
+  describe "GET /api/v1/merchants/:merchant_id/invoices/:id" do
+    it "returns an invoice with coupon id" do
+      coupon = create(:coupon, merchant: @merchant1)
+      invoice = create(:invoice, customer: @customer1, merchant: @merchant1, coupon: coupon)
+  
+      get "/api/v1/merchants/#{@merchant1.id}/invoices/#{invoice.id}"
+  
+      expect(response).to be_successful
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:data][:attributes][:coupon_id]).to eq(coupon.id)
+    end
+  
+    it "returns an invoice without a coupon id" do
+      invoice = create(:invoice, customer: @customer1, merchant: @merchant1)
+  
+      get "/api/v1/merchants/#{@merchant1.id}/invoices/#{invoice.id}"
+  
+      expect(response).to be_successful
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:data][:attributes][:coupon_id]).to be_nil
     end
   end
 end
